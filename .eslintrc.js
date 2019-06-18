69bcd1a464f6eaf9b334c613e2caee2b9c889598
Alasdair Collinson <blalasaadri@googlemail.com>
2019-06-18 23:09:17 +0000

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true
+  },
+  extends: ["plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module",
+    ecmaFeatures: {
+      impliedStrict: true
+    }
+  },
+  rules: {
+    "prettier/prettier": "error",
+    strict: ["error", "global"],
+    "no-undef": ["error"]
+  },
+  overrides: [
+    {
+      files: ["**/*.test.js"],
+      env: {
+        jest: true
+      },
+      plugins: ["jest"],
+      rules: {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error"
+      }
+    }
+  ]
+};
